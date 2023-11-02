1ae4daff1d499a9fff9e6881568833b115af4929
Mudassir Riaz <riazmudassir64@gmail.com>
2023-11-02 20:32:56 +0500

    Changes done in home

--- Open Ended Lab 1/Task 1.cpp
+++ Open Ended Lab 1/Task 1.cpp
@@ -4,49 +4,49 @@ using namespace std;
 int main()
 {
 	string username, bookname;
-	int borrowdate, returndate=0, remainderdate,fine,todaydate;
+	int  days,fine;
 	char choice, finechoice;
 	cout << "Enter your name : ";
 	getline(cin, username);
 	cout << "Library System Main Menu " << endl;
 	cout << "1. Borrow" << endl;
-	cout << "2. Return" << endl;
+	cout << "2. Return" << endl; 
 	cout << "Press Q to quit" << endl;
 	cout << "Enter your choice : ";
 	cin >> choice;
 	if (choice == '1')
 	{
 		cout << "Enter the name of the book you want to borrow : ";
-		cin >> bookname;
-		cout << "Enter the date of the day on which you want to return the book(30 days) : ";
-		cin>>returndate;
-		cout << "Congratulations!, you have borrowed the book " << bookname << endl;
-		cout << "Ensure to return the date before the due date" << endl;
+		cin.ignore();
+		getline(cin, bookname);
+		cout << "Congratulations!, you have borrowed the book " <<"\""<< bookname<<"\"" << endl;
+		cout << "Ensure to return the book in 3 days otherwise fine will be imposed of 1$ per day." << endl;
 	}
 	else if (choice == '2')
 	{
 		cout << "Enter the book name which you want to return : ";
-		cin>>bookname;
-		cout << "Enter the date you borrowed the book ";
-		cin >> borrowdate;
-		cout << "Enter today date ";
-		cin >> todaydate;
-		remainderdate = borrowdate - todaydate;
-		cout << " Oops!, looks like you came late so there will be a fine, it will 1$ a day. " << endl;
-		cout << "Days Late = " << remainderdate << endl;
-		fine = 1 * remainderdate;
-		cout << "Your total fine will be = " << fine <<"$"<< endl;
-		cout << " Please Pay fine first "<< endl;
-		cout << "Do you want to pay the fine(Y/N): ";
-		cin >> finechoice;
-		if (finechoice == 'Y' || finechoice == 'y')
+		cin.ignore();
+		getline(cin,bookname);
+		cout << "From how many days, you had the book?"<< endl;
+		cin >> days;
+		if (days > 3) //Limit is 3 days, if return date is greater than 3 days, fine will be imposed
 		{
-			cout << "Fine paid!, now you can return the book ";
+			fine = days - 3;
+			cout << "Oops!, looks like you are late, so there will be a fine(there was a due date of 3 days)" << endl;
+			cout << "Your total fine will be = " << fine << "$" << endl;
+			cout << "Please Pay fine first " << endl;
+			cout << "Do you want to pay the fine(Y/N): ";
+			cin >> finechoice;
+			if (finechoice == 'Y' || finechoice == 'y')
+			{
+				cout << "Fine paid!, you can now return the book " << endl;
+			}
+			else if (finechoice == 'N' || finechoice == 'n')
+			{
+				cout << "It is compulsory to pay the fine." << endl;
+			}
 		}
-		else if (finechoice == 'N' || finechoice == 'n')
-		{
-			cout << "It is compulsory to pay the fine." << endl;
-		}
+		
 	}
 	else if (choice == 'q' || choice == 'Q' || choice == '3')
 	{
